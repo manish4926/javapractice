1e957d406d1ef1e028db3767321f6b471c758fa3
Manish Arora <manish.arora4926@gmail.com>
2024-02-29 11:03:14 +0530

    target

--- src/Sorting.java
+++ src/Sorting.java
@@ -4,13 +4,53 @@ public class Sorting {
 
     public static void main(String[] args) {
         Sorting st = new Sorting();
-        st.program1(); //bubble sort
+        //st.program1(); //bubble sort
+        st.program2(); //selection sort
+    }
+
+    public void program2() {
+        int[] arr = {7,8,3,1,2};
+        String type = "greater";
+        int temp = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            switch (type) {
+                case "greater":
+                    int greatest = i;
+                    for (int j = 0; j < arr.length-1-i; j++) {
+                        if(arr[j] > arr[j+1]) {
+                            greatest = i;
+                        }
+                    }
+                    temp = arr[greatest];
+                    arr[greatest] = arr[i];
+                    arr[i] = temp;
+                    break;
+
+                case "smaller":
+                    int smallest = i;
+                    for (int j = i+1; j < arr.length; j++) {
+                        if(arr[smallest] > arr[j]) {
+                            smallest = j;
+                        }
+                    }
+                    temp = arr[smallest];
+                    arr[smallest] = arr[i];
+                    arr[i] = temp;
+
+                    break;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
     }
 
     public void program1() {
         int[] arr = {7,8,3,1,2};
         String type = "smaller";
         //int[] arr1 = new int[arr.length];
+        //Time Complexity = 0(n^2) //Not a good approach
         int temp = 0;
         for (int i = 0; i < arr.length-1; i++) {
             switch (type) {
